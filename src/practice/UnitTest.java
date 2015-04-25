57c157da3a117ba178c216f57c5d860348deaf5b
haroldalmon <haroldemail@gmail.com>
2015-04-25 16:34:01 -0500

    Initail version.

---
+++ src/practice/UnitTest.java
@@ -0,0 +1,22 @@
+package practice;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class UnitTest {
+	VeryLargeNumberSort veryLargeNumberSort = VeryLargeNumberSort.getInstance();
+	
+	@Test(timeout = 500)
+	public void VeryLargeNumberSort() {
+//		VeryLargeNumberSort veryLargeNumberSort = VeryLargeNumberSort.getInstance();
+		System.out.printf("sortFileandReturnNumber(\"oneThousand.bin\", 1_000, 100) = %d%n", veryLargeNumberSort.sortFileandReturnNumber("oneThousand.bin", 1_000, 100));
+	}
+	
+	private final int twoHours = 2 * 3_600;
+	@Test(timeout = twoHours)
+	@Ignore
+	public void SortOneBillionNumbers() {
+		// 1 Billion...THIS IS SLOW, 45 minutes on an i7 core!
+		System.out.printf("sortFileandReturnNumber(\"oneBillion.bin\", 1_000_000_000, 1_000_000) = %d%n", veryLargeNumberSort.sortFileandReturnNumber("oneBillion.bin", 1_000_000_000, 1_000_000));
+	}
+}
